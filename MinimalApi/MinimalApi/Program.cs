20c07c170967ff16a41abed45eecd5292fc098fa
santiMoreno1989 <santiagom@gmail.com>
2022-10-05 21:49:46 -0300

    Creacion api minial y crud

---
+++ MinimalApi/MinimalApi/Program.cs
@@ -0,0 +1,65 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TodoDbContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/todoItems", async (Todo todo, TodoDbContext dbContext)
+    =>{
+    dbContext.Todos.Add(todo);
+    await dbContext.SaveChangesAsync();
+    return Results.Created($"/todoItems{todo.Id}", todo);
+});
+
+app.MapGet("/todoItems", async (TodoDbContext dbContext)
+    => await dbContext.Todos.ToListAsync());
+
+app.MapGet("/todoItems/complete", async (TodoDbContext dbContext)
+    => await dbContext.Todos.Where(t=> t.IsComplete==true).ToListAsync());
+
+app.MapGet("/todoItems/{id}", async (int id, TodoDbContext dbContext) 
+    => await dbContext.Todos.FindAsync(id)
+        is Todo todo ? Results.Ok(todo) : Results.NotFound());
+
+app.MapPut("/todoImtes/{id}", async(int id,Todo inputTodo, TodoDbContext dbContext) 
+    => {
+        var todo = await dbContext.Todos.FindAsync(id);
+        
+        if (todo is null) return Results.NotFound();
+        
+        todo.Name=inputTodo.Name;
+        todo.IsComplete=inputTodo.IsComplete;
+        await dbContext.SaveChangesAsync();
+
+        return Results.NoContent();
+    });
+
+app.MapDelete("/todoItems/{id}", async (int id,TodoDbContext dbContext)
+    => {
+        if (await dbContext.Todos.FindAsync(id) is Todo todo) 
+        {
+            dbContext.Todos.Remove(todo);
+            await dbContext.SaveChangesAsync();
+            return Results.Ok(todo);
+        }
+        return Results.NotFound();  
+    });
+
+app.Run();
---
+++ MinimalApi/MinimalApi/TodoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalApi
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
